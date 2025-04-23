ec22952f1b4fe5fb12681520a3f079aeb4ff257a
oanagrecu <oana.alexandra.grecu@gmail.com>
2025-04-23 20:37:57 +0200

    Implement function to copy files from 'files' to 'files_copy' folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import {copyFile, mkdir, readdir, stat} from "fs/promises";
+import path from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+	const sourceFolder = path.join(__dirname, "files");
+	const destinationFolder = path.join(__dirname, "files_copy");
+
+	try {
+		await stat(sourceFolder);
+	} catch (err) {
+		throw new Error("FS operation failed");
+	}
+
+	try {
+		await stat(destinationFolder);
+		throw new Error("FS operation failed");
+	} catch (err) {
+		if (err.code !== "ENOENT") {
+			throw new Error("FS operation failed");
+		}
+	}
+
+	await mkdir(destinationFolder, {recursive: true});
+
+	const files = await readdir(sourceFolder);
+
+	for (const file of files) {
+		const sourceFilePath = path.join(sourceFolder, file);
+		const destinationFilePath = path.join(destinationFolder, file);
+
+		await copyFile(sourceFilePath, destinationFilePath);
+	}
+
+	console.log("Files copied successfully!");
 };
 
 await copy();
