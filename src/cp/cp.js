e0eb048d84abbb3914618b2dfee130139ab1b3de
oanagrecu <oana.alexandra.grecu@gmail.com>
2025-04-27 19:31:28 +0200

    Child Processes (src/cp) implement several functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import {spawn} from "child_process";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const scriptPath = path.join(__dirname, "./files/script.js");
+
+	const child = spawn("node", [scriptPath, ...args]);
+
+	process.stdin.pipe(child.stdin);
+
+	child.stdout.pipe(process.stdout);
+
+	child.stderr.pipe(process.stderr);
+
+	child.on("exit", (code) => {
+		console.log(`Child process exited with code ${code}`);
+	});
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3"]);
